04180018ec4fce948dd69c68ba11917de5ffb7eb
efany <18200179234@163.com>
2016-03-10 22:00:18 +0800

    commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.efan.okhttputils.activity.BaseActivity;
+import com.efan.okhttputils.annotate.BindView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    public void setRootView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
