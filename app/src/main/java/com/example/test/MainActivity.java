91e9211bfcff92bb1c8af370b9506051a77dd9c6
efany <18200179234@163.com>
2016-03-13 21:58:49 +0800

    uegbb

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.efan.okhttputils.activity.BaseActivity;
 import com.efan.okhttputils.annotate.BindView;
@@ -11,6 +12,8 @@ import com.efan.okhttputils.annotate.BindView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
